f63785d2ad5235f1f3a50f6d8a8cc42d0a2c2c1d
Zachary Carter <zack.carter@gmail.com>
2023-07-03 10:33:38 -0700

    wip

--- packages/relay/src/relay.js
+++ packages/relay/src/relay.js
@@ -8,8 +8,27 @@ import { circuitRelayServer } from "libp2p/circuit-relay";
 import { webSockets } from "@libp2p/websockets";
 import * as filters from "@libp2p/websockets/filters";
 import { identifyService } from "libp2p/identify";
+import { keys } from "@libp2p/crypto";
+import { createFromPrivKey } from "@libp2p/peer-id-factory";
+import { fromString as uint8ArrayFromString } from "uint8arrays/from-string";
+//import { toString as uint8ArrayToString } from "uint8arrays/to-string";
+import "dotenv/config";
+
+let peerId;
+if (process.env.PEER_SECRET) {
+  peerId = await createFromPrivKey(
+    await keys.unmarshalPrivateKey(
+      uint8ArrayFromString(process.env.PEER_SECRET, "base64pad")
+    )
+  );
+} else {
+  const key = await keys.generateKeyPair("Ed25519");
+  peerId = await createFromPrivKey(key);
+  //console.log(uint8ArrayToString(keys.marshalPrivateKey(key), "base64pad"));
+}
 
 const server = await createLibp2p({
+  peerId,
   addresses: {
     listen: ["/ip4/0.0.0.0/tcp/59351/ws", "/ip4/0.0.0.0/tcp/59353"],
   },
